cf327b6c331e9fa5e9da2eb0e715c2601163760a
shubhi1928 <66595223+shubhi1928@users.noreply.github.com>
2021-05-12 21:37:33 +0530

    Responsive features

---
+++ script.js
@@ -0,0 +1,25 @@
+window.onscroll = function () { myFunction() };
+
+var navigation = document.getElementById("myTopnav");
+var sticky = navigation.offsetTop;
+
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navigation.classList.add("sticky");
+        navigation.classList.remove("responsive");
+
+    } else {
+        navigation.classList.remove("sticky");
+    }
+}
+
+function mynav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+
+}
